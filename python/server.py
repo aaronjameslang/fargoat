23b2378d06cd8c1f0cdf089ce370496ae73a0d68
Aaron J. Lang <aaronjameslang@googlemail.com>
2020-12-09 19:22:49 -0800

    add server

---
+++ python/server.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+# pip install flask
+import flask
+import func
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    url = "/message?name=Apollo"
+    return "Try GET <a href=\"{u}\">{u}</a>".format(u=url)
+
+@app.route('/message', methods=['GET'])
+def message():
+    return func.get_message('Apollo')
+
+app.run()
