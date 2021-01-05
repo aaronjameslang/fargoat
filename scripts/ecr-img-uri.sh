72f496b3a865f573efbaf7f87bf1ce6bb240e5a1
Aaron J. Lang <aaronjameslang@googlemail.com>
2021-01-04 22:51:18 -0800

    WIP rm mess

--- scripts/ecr-img-uri.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-set -eu
-
-. ./.env
-
-ecr_uri=$(./scripts/ecr-uri.sh)
-version=$(./scripts/version.sh)
-
-echo $ecr_uri/$PROJECT_NAME-$STAGE:$version
--- scripts/ecr-uri.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-set -eu
-
-. ./.env
-
-account_id=$(./scripts/account-id.sh)
-
-echo $account_id.dkr.ecr.$REGION.amazonaws.com
